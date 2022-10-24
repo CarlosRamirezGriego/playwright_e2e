51bf9561a817117eb0d253acbe81f37aca626bf6
Carlos Ramirez <32551875+CarlosRamirezGriego@users.noreply.github.com>
2022-10-24 09:20:29 -0700

    Updated locators

--- tests/test_pokemon.spec.ts
+++ tests/test_pokemon.spec.ts
@@ -5,21 +5,22 @@ import { T001_TestData } from "../test_data/test_pokemon"
 test('Print the Pokemon Data main menu label @T001', async ({ page, isMobile }) => {
   await page.goto('https://pokemondb.net/');
 
-  // Expect a title "to contain" a substring.
+  // Expect title to have this Name
   await expect(page).toHaveTitle("Pokémon Database -- the fastest way to get your Pokémon information");
 
   // Find "Data" element menu and click it
 
-  //let links: Locator = await page.locator(MainLinks_HomePage(isMobile).POKEMON_DATA_LABEL);
-  let links: Locator = await page.locator('.main-menu-list li:nth-child(1) .main-menu-title-long');
+  let links: Locator = await page.locator(MainLinks_HomePage(isMobile).POKEMON_DATA_LINK);
+  let link_label: Locator = await page.locator(MainLinks_HomePage(isMobile).POKEMON_DATA_LABEL);
+  //let links: Locator = await page.locator('.main-menu-list li:nth-child(1) .main-menu-title-long');
   
-  //let expectedLabel: string = await T001_TestData(isMobile)
-  let expectedLabel: string = "Pokémon data"
-  let actualText: string = await links.innerText()
+  let expectedLabel: string = await T001_TestData(isMobile)
+  //let expectedLabel: string = "Pokémon data"
+  let actualText: string = await link_label.innerText()
   
   await links.click()
 
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   
   await expect(actualText).toEqual(expectedLabel)
 });
--- ui_elements/home_page.ts
+++ ui_elements/home_page.ts
@@ -2,13 +2,15 @@ export function MainLinks_HomePage(isWeb: boolean){
     if(isWeb)
     {
         return{
-            POKEMON_DATA_LABEL: '.main-menu-list li:nth-child(1) .main-menu-title-long'
+            POKEMON_DATA_LINK: '.main-menu-list>li:nth-child(1)>a',
+            POKEMON_DATA_LABEL: '.main-menu-list>li:nth-child(1) .main-menu-title-long'
         }
     }
     else
     {
         return{
-            POKEMON_DATA_LABEL: '.main-menu-list li:nth-child(1) .main-menu-title'
+            POKEMON_DATA_LINK: '.main-menu-list>li:nth-child(1)>a',
+            POKEMON_DATA_LABEL: '.main-menu-list>li:nth-child(1) .main-menu-title'
         }
     }	
 }
